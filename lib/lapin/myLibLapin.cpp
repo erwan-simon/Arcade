c1df0b983b91518cded77362addeba5d1ad3832f
Selim Rinaz <selim.rinaz@epitech.eu>
2017-03-31 16:19:47 +0200

    les couleurs c pt

--- lib/lapin/myLibLapin.cpp
+++ lib/lapin/myLibLapin.cpp
@@ -5,7 +5,7 @@
 ** Login   <selimrinaz@epitech.net>
 ** 
 ** Started on  Tue Mar 28 17:42:54 2017 Selim Rinaz
-** Last update Fri Mar 31 15:16:21 2017 Selim Rinaz
+** Last update Fri Mar 31 16:18:43 2017 Selim Rinaz
 */
 
 #include <unistd.h>
@@ -46,17 +46,41 @@ myLibLapin::e_key	myLibLapin::getKey()
   return (E_NONE);
 }
 
+void            fill_color(t_bunny_pixelarray *pixarray, void *color)
+{
+  int           idx;
+  int           total;
+  unsigned      *pixels;
+
+  idx = 0;
+  pixels = (unsigned *)pixarray->pixels;
+  total = pixarray->clipable.clip_width * pixarray->clipable.clip_height;
+  while (idx < total)
+    {
+      pixels[idx] = *color;
+      idx++;
+    }
+}
+
 int			myLibLapin::buildCell(int x, int y, e_color col)
 {
   t_bunny_position	position;
   t_bunny_pixelarray	*pixarray;
   t_bunny_window	*window;
 
-  (void)col;
   if (NULL == (pixarray = bunny_new_pixelarray(20, 20)))
     return (1);
   position.x = x * 20;
   position.y = y * 20;
+  switch (col) {
+  case (E_BLACK) : fill_color(pixarray, BLACK);
+  case (E_WHITE) : fill_color(pixarray, WHITE);
+  case (E_YELLOW) : fill_color(pixarray, YELLOW);
+  case (E_RED) : fill_color(pixarray, RED);
+  case (E_BLUE) : fill_color(pixarray, BLUE);
+  case (E_PINK) : fill_color(pixarray, PINK);
+  case (E_GREEN) : fill_color(pixarray, GREEN);
+    }
   bunny_blit(&this->window->buffer, &pixarray->clipable, &position);
   bunny_display(this->window);
   return (0);
@@ -69,7 +93,7 @@ int		main()
   lapin.openWindow(40, 40);
   lapin.clearWindow();
   lapin.buildCell(3, 3, myLibLapin::E_BLACK);
-  sleep(10);
+  sleep(5);
   lapin.closeWindow();
   return (EXIT_SUCCESS);
 }
